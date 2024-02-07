086e3d2db1b370e8114b37152fff6745bdaf6a50
Firat Yedibela <yedibelafirat0@gmail.com>
2024-02-07 13:57:49 +0300

    Implement renderLibrary() function

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const myLibrary = [{
   status: true,
 }];
 
+renderLibrary();
+
 function Book(name, pages, author, status) {
   this.name = name;
   this.pages = pages;
@@ -18,11 +20,17 @@ function Book(name, pages, author, status) {
 }
 
 document.querySelector('.add-book-btn').addEventListener('click', (e) => {
+  // Prevent the button from submitting the form and reloading the page
   e.preventDefault()
+
   addBookToLibrary();
   renderLibrary();
   emptyForm();
 });
+
+
+
+
 
 function addBookToLibrary() {
   // Get book's properties
@@ -35,4 +43,34 @@ function addBookToLibrary() {
   const book = new Book(name, author, pages, status);
   myLibrary.push(book);
   console.table(myLibrary);
+}
+
+function renderLibrary() {
+  // First empty the table, then recreate table html to avoid duplication
+  document.querySelector('#table-body').innerHTML = '';
+  
+  // Iterate through myLibrary, generate html for each book, add html to #table-body, and then fill that html with data
+  myLibrary.forEach((book) => {
+    // For each book, create the cells and fill the cells with data
+    /*
+      Make cell's ids are the same as the book object's properties, that way the cell will get
+      the right value for itself > cell.textContext = book[cell.id];
+    */
+    document.querySelector('#table-body').innerHTML += `
+      <tr>
+        <td id="name"></td>
+        <td id="author"></td>
+        <td id="pages"></td>
+        <td id="status"></td>
+      </tr>
+    `;
+    // Iterate through all the cells that's just created and give them the current book object's data
+    const cells = Array.from(document.querySelectorAll('#table-body > tr:last-child > td'));
+    cells.forEach((cell) => {
+      cell.textContent = book[cell.id];
+    });
+    
+    // Update status cell's content to display Read or Not Read instead of true/false
+    cells[cells.length - 1].textContent = book.status ? 'Read' : 'Not Read';
+  });
 }
